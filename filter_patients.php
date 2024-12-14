a7aa4d177f27d303727c4202db3065a6a4af2fbb
Cheese Burger <Cheese Burger@DESKTOP-FO7BL9D>
2024-12-14 15:34:38 +0000

    bug: fixed neighbourhood default value to send empty string to return all neighbourhoods.

--- filter_patients.php
+++ filter_patients.php
@@ -5,8 +5,6 @@ header('Content-Type: application/json');
 
 $filters = json_decode(file_get_contents('php://input'), true);
 
-error_log('Filters received: ' . print_r($filters, true));
-
 $db = new Database();
 $patients = $db->database->Patients;
 
@@ -45,14 +43,11 @@ if ($filters['ScholarshipTrue'] && !$filters['ScholarshipFalse']) {
 }
 
 // Neighbourhood Filter
-if (!empty($filters['Neighbourhood']) && $filters['Neighbourhood'] !== 'All') {
-    $query['Patient.Neighbourhood'] = $filters['Neighbourhood'];
+if (!empty($filters['Neighbourhood'])) {
+    $query['Neighbourhood'] = $filters['Neighbourhood'];
 }
-
-// Log the query
-error_log('MongoDB Query: ' . print_r($query, true));
 
 // Execute the query
 $result = $patients->find($query);
 echo json_encode(iterator_to_array($result));
-?>
+
