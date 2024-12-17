2cb71d1f175657e5d5a01789ffd765d15746aa46
Cheese Burger <Cheese Burger@DESKTOP-FO7BL9D>
2024-12-17 10:02:56 +0000

    docs: added comments to phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,6 @@
 <?php
-phpinfo();
+phpinfo(); // This will display all the PHP information when the page is run
+// Useful for debugging and checking the PHP configuration
+// Originally used when setting up the server to check if PHP is installed and running
+// Also to check if MongoDB is installed and running
 ?>
